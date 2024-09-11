407a9c1761e414cda62ac5cbcf992d388ab9867c
halizaarfa <halizansa@gmail.com>
2024-09-11 14:05:41 +0700

    Tutorial 2: Form dan Data Delivery - Fixing PWS URL in CSRF_TRUSTED_ORIGINS

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://haliza.nafiah/mentalhealthtracker", "https://haliza.nafiah/mentalhealthtracker"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://haliza-nafiah-mentalhealthtracker", "https://haliza-nafiah-mentalhealthtracker"]
