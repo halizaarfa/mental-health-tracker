dda611dac1b1ca739b6ed1903d262d5cfc199e57
halizaarfa <halizansa@gmail.com>
2024-09-18 14:07:54 +0700

    Tutorial 3: Autentikasi, Session, dan Cookies - LFH alias Lab from Home :(

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=yc(8yanqnrdbu=y1shnhuv9f07*#ru&yw=(bpc86h*u#9ts9r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "haliza-nafiah-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
